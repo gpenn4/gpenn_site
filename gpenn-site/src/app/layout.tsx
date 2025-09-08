a3baa3bc718df2d86f9dc32153d4333ac07a6e77
gpenn4 <georgiapennington4@gmail.com>
2025-09-08 14:49:52 +0100

    Bug Fix

--- gpenn-site/src/app/layout.tsx
+++ gpenn-site/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Image from 'next/image'
+import Link from "next/link";
 
 
 export const metadata: Metadata = {
@@ -14,23 +15,23 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="min-h-dvh bg-white text-gray-900 antialiased">
         <header className="border-b">
           <nav className="mx-auto max-w-6xl px-4 py-4 flex items-center justify-between">
-            <a href="/" className="font-semibold">
+            <Link href="/" className="font-semibold">
               <Image src="/icon.png" alt="GP" width={30}
                 height={30} />
-            </a>
+            </Link>
             <div className="space-x-5 text-sm flex">
-              <a href="/projects" className="hover:underline">Projects</a>
-              <a href="/about" className="hover:underline">About Me</a>
-              <a href="/photography" className="hover:underline">Photography</a>
+              <Link href="/projects" className="hover:underline">Projects</Link>
+              <Link href="/about" className="hover:underline">About Me</Link>
+              <Link href="/photography" className="hover:underline">Photography</Link>
               <h5 className="bold">|</h5>
-              <a href="https://www.linkedin.com/in/georgia-pennington-903823188/">
+              <Link href="https://www.linkedin.com/in/georgia-pennington-903823188/">
                 <Image src="/icons/linkedin.png" alt="LinkedIn" width={17}
                   height={17} />
-              </a>
-              <a href="https://github.com/gpenn4">
+              </Link>
+              <Link href="https://github.com/gpenn4">
                 <Image src="/icons/GitHub_Logo.png" alt="GitHub" width={42}
                   height={50} />
-              </a>
+              </Link>
             </div>
           </nav>
         </header>
