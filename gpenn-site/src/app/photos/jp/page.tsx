b82d6a07e3d0fee6ebc3d27426bc9e42346f52f4
gpenn4 <georgiapennington4@gmail.com>
2025-09-11 00:38:45 +0100

    Style: updata japan page

--- gpenn-site/src/app/photos/jp/page.tsx
+++ gpenn-site/src/app/photos/jp/page.tsx
@@ -9,9 +9,13 @@ export default function JapanPage() {
                 height={100} />
             <Image src="/photos/japan/tokyo3.jpg" alt="" width={300}
                 height={100} />
+            <Image src="/photos/japan/tokyo12.jpg" alt="" width={125}
+                height={100} />
             <Image src="/photos/japan/tokyo4.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/japan/tokyo5.jpg" alt="" width={300}
+                height={100} />
+            <Image src="/photos/japan/tokyo14.jpg" alt="" width={125}
                 height={100} />
             <Image src="/photos/japan/tokyo6.jpg" alt="" width={300}
                 height={100} />
@@ -19,11 +23,17 @@ export default function JapanPage() {
                 height={100} />
             <Image src="/photos/japan/tokyo9.jpg" alt="" width={300}
                 height={100} />
+            <Image src="/photos/japan/tokyo15.jpg" alt="" width={125}
+                height={100} />
             <Image src="/photos/japan/tokyo8.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/japan/tokyo10.jpg" alt="" width={300}
                 height={100} />
+            <Image src="/photos/japan/tokyo13.jpg" alt="" width={300}
+                height={100} />
             <Image src="/photos/japan/tokyo11.JPG" alt="" width={300}
+                height={100} />
+            <Image src="/photos/japan/tokyo16.jpg" alt="" width={125}
                 height={100} />
 
             <Image src="/photos/japan/japan1.jpg" alt="" width={300}
@@ -32,9 +42,13 @@ export default function JapanPage() {
                 height={100} />
             <Image src="/photos/japan/japan3.JPG" alt="" width={300}
                 height={100} />
+            <Image src="/photos/japan/japan7.jpg" alt="" width={125}
+                height={100} />
             <Image src="/photos/japan/japan4.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/japan/japan5.jpg" alt="" width={300}
+                height={100} />
+            <Image src="/photos/japan/japan6.jpg" alt="" width={125}
                 height={100} />
 
             <Image src="/photos/japan/kyoto1.jpg" alt="" width={300}
@@ -45,8 +59,16 @@ export default function JapanPage() {
                 height={100} />
             <Image src="/photos/japan/kyoto4.JPG" alt="" width={300}
                 height={100} />
-            {/* portrait photos */}
-            
+            <Image src="/photos/japan/kyoto5.jpg" alt="" width={125}
+                height={100} />
+            <Image src="/photos/japan/kyoto6.jpg" alt="" width={300}
+                height={100} />
+            <Image src="/photos/japan/kyoto7.jpg" alt="" width={300}
+                height={100} />
+            <Image src="/photos/japan/kyoto8.jpg" alt="" width={125}
+                height={100} />
+            <Image src="/photos/japan/kyoto9.jpg" alt="" width={300}
+                height={100} />
         </div>
 
     </div>
