4aa6ee78dc7ee654b6ff193c9334c31bc8287487
gpenn4 <georgiapennington4@gmail.com>
2025-10-21 13:44:48 -0400

    Style: Italy page

--- gpenn-site/src/app/photos/it/page.tsx
+++ gpenn-site/src/app/photos/it/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function ItalyPage() {
     return <div>
         <div className="gallery gap-4">
-            <Image src="/photos/italy/genoa1.JPG" alt="" width={300}
+            <Image src="/photos/italy/genoa9.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/italy/genoa2.JPG" alt="" width={300}
                 height={100} />
@@ -12,6 +12,20 @@ export default function ItalyPage() {
             <Image src="/photos/italy/genoa3.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/italy/genoa5.JPG" alt="" width={125}
+                height={100} />
+            <Image src="/photos/italy/genoa6.JPG" alt="" width={300}
+                height={100} />
+            {/* <Image src="/photos/italy/genoa7.JPG" alt="" width={300}
+                height={100} /> */}
+            <Image src="/photos/italy/genoa8.JPG" alt="" width={300}
+                height={100} />
+            <Image src="/photos/italy/genoa1.jpg" alt="" width={300}
+                height={100} />
+            <Image src="/photos/italy/genoa10.JPG" alt="" width={300}
+                height={100} />
+            <Image src="/photos/italy/genoa11.JPG" alt="" width={300}
+                height={100} />
+            <Image src="/photos/italy/genoa12.jpg" alt="" width={300}
                 height={100} />
         </div>
 
