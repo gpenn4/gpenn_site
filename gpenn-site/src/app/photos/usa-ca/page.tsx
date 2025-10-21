a38be9d2d851ed8793a7f47643f06d693156cb74
gpenn4 <georgiapennington4@gmail.com>
2025-10-21 14:55:52 -0400

    Style: usa page

--- gpenn-site/src/app/photos/usa-ca/page.tsx
+++ gpenn-site/src/app/photos/usa-ca/page.tsx
@@ -5,8 +5,8 @@ export default function USPage() {
         <div className="gallery gap-4">
             <Image src="/photos/usa/Aspen3.jpg" alt="" width={300}
                 height={100} />
-            <Image src="/photos/usa/Aspen4.jpg" alt="" width={300}
-                height={100} />
+            {/* <Image src="/photos/usa/Aspen4.jpg" alt="" width={300}
+                height={100} /> */}
             <Image src="/photos/usa/Aspen2.jpg" alt="" width={300}
                 height={100} />
             <Image src="/photos/usa/Aspen5.JPG" alt="" width={300}
@@ -15,11 +15,13 @@ export default function USPage() {
                 height={100} />
             <Image src="/photos/usa/aspen6.JPG" alt="" width={300}
                 height={100} />
-            <Image src="/photos/usa/aspen7.JPG" alt="" width={300}
-                height={100} />
             <Image src="/photos/usa/aspen8.JPG" alt="" width={125}
                 height={100} />
             <Image src="/photos/usa/aspen9.jpg" alt="" width={300}
+                height={100} />
+            <Image src="/photos/usa/aspen10.JPG" alt="" width={300}
+                height={100} />
+            <Image src="/photos/usa/aspen7.JPG" alt="" width={300}
                 height={100} />
 
             <Image src="/photos/usa/gwich1.JPG" alt="" width={300}
@@ -46,6 +48,18 @@ export default function USPage() {
             <Image src="/photos/usa/ny4.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/usa/ny5.JPG" alt="" width={300}
+                height={100} />
+            <Image src="/photos/usa/ny6.jpg" alt="" width={300}
+                height={100} />
+            <Image src="/photos/usa/ny7.jpg" alt="" width={300}
+                height={100} />
+            <Image src="/photos/usa/ny8.jpg" alt="" width={300}
+                height={100} />
+            <Image src="/photos/usa/ny10.jpg" alt="" width={300}
+                height={100} />
+            <Image src="/photos/usa/ny11.jpg" alt="" width={125}
+                height={100} />
+            <Image src="/photos/usa/ny12.jpg" alt="" width={125}
                 height={100} />
 
             <Image src="/photos/usa/nh1.jpg" alt="" width={300}
