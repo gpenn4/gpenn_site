566ae2db8766a163975b15944a2356b1173ad8bc
gpenn4 <georgiapennington4@gmail.com>
2025-10-21 14:36:13 -0400

    Style: belgium page

--- gpenn-site/src/app/photos/be/page.tsx
+++ gpenn-site/src/app/photos/be/page.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 export default function BelgiumPage() {
     return <div>
         <div className="gallery gap-4">
-            <Image src="/photos/belgium/brussels2.JPG" alt="" width={300}
-                height={100} />
+            {/* <Image src="/photos/belgium/brussels2.JPG" alt="" width={300}
+                height={100} /> */}
             <Image src="/photos/belgium/brussels4.jpg" alt="" width={300}
                 height={100} />
             <Image src="/photos/belgium/brussels5.JPG" alt="" width={300}
@@ -19,13 +19,15 @@ export default function BelgiumPage() {
                 height={100} />
             <Image src="/photos/belgium/brussels9.JPG" alt="" width={300}
                 height={100} />
-            
-            {/* <Image src="/photos/belgium/brussels3.JPG" alt="" width={300}
-                height={100} /> */}
-
             <Image src="/photos/belgium/brussels1.jpg" alt="" width={125}
                 height={100} />
+            <Image src="/photos/belgium/brussels3.JPG" alt="" width={300}
+                height={100} />
             <Image src="/photos/belgium/brussels10.jpg" alt="" width={125}
+                height={100} />
+            <Image src="/photos/belgium/brussels12.jpg" alt="" width={125}
+                height={100} />
+            <Image src="/photos/belgium/brussels13.jpg" alt="" width={125}
                 height={100} />
         </div>
 
