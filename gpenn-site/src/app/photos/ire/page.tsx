91e3cd38147728a6f7c8e3be14a540f7c8335a72
gpenn4 <georgiapennington4@gmail.com>
2025-10-21 14:23:57 -0400

    Style: ireland page

--- gpenn-site/src/app/photos/ire/page.tsx
+++ gpenn-site/src/app/photos/ire/page.tsx
@@ -3,7 +3,16 @@ import Image from 'next/image'
 export default function IrelandPage() {
     return <div>
         <div className="gallery gap-4">
-            <Image src="/photos/ireland/ire1.jpg" alt="" width={300}
+            <Image src="/photos/ireland/ire1.JPG" alt="" width={300}
+                height={100} />
+            <Image src="/photos/ireland/ire2.JPG" alt="" width={125}
+                height={100} />
+            {/* <Image src="/photos/ireland/ire3.JPG" alt="" width={300}
+                height={100} /> */}
+            <Image src="/photos/ireland/ire4.JPG" alt="" width={125}
+                height={100} />
+
+            <Image src="/photos/ireland/dublin1.jpg" alt="" width={300}
                 height={100} />
             <Image src="/photos/ireland/dublin2.jpg" alt="" width={300}
                 height={100} />
@@ -16,6 +25,8 @@ export default function IrelandPage() {
             <Image src="/photos/ireland/dublin6.jpg" alt="" width={300}
                 height={100} />
             <Image src="/photos/ireland/dublin7.JPG" alt="" width={300}
+                height={100} />
+            <Image src="/photos/ireland/dublin8.JPG" alt="" width={125}
                 height={100} />
 
         </div>
