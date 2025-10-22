d788882d378fb3ddf7c6203dbe003b6a0be7fa1c
gpenn4 <georgiapennington4@gmail.com>
2025-10-22 11:13:51 -0400

    Style: japan page

--- gpenn-site/src/app/photos/jp/page.tsx
+++ gpenn-site/src/app/photos/jp/page.tsx
@@ -3,15 +3,20 @@ import Image from 'next/image'
 export default function JapanPage() {
     return <div>
         <div className="gallery gap-4">
+            <h1 className="text-xl font-bold py-20">Tokyo</h1>
             <Image src="/photos/japan/tokyo1.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/japan/tokyo2.JPG" alt="" width={300}
+                height={100} />
+            <Image src="/photos/japan/tokyo15.jpg" alt="Little boy on boat" width={125}
                 height={100} />
             <Image src="/photos/japan/tokyo3.jpg" alt="" width={300}
                 height={100} />
             <Image src="/photos/japan/tokyo12.jpg" alt="" width={125}
                 height={100} />
             <Image src="/photos/japan/tokyo4.JPG" alt="" width={300}
+                height={100} />
+            <Image src="/photos/japan/tokyo19.jpg" alt="Couple with umbrella" width={130}
                 height={100} />
             <Image src="/photos/japan/tokyo5.jpg" alt="" width={300}
                 height={100} />
@@ -21,55 +26,71 @@ export default function JapanPage() {
                 height={100} />
             <Image src="/photos/japan/tokyo7.jpg" alt="" width={300}
                 height={100} />
-            <Image src="/photos/japan/tokyo9.jpg" alt="" width={300}
-                height={100} />
-            <Image src="/photos/japan/tokyo15.jpg" alt="" width={125}
+            <Image src="/photos/japan/tokyo9.jpg" alt="Girl on boat" width={300}
                 height={100} />
             <Image src="/photos/japan/tokyo8.JPG" alt="" width={300}
                 height={100} />
-            <Image src="/photos/japan/tokyo10.jpg" alt="" width={300}
+            <Image src="/photos/japan/tokyo10.jpg" alt="Nezu Museum" width={300}
                 height={100} />
-            <Image src="/photos/japan/tokyo13.jpg" alt="" width={300}
+            <Image src="/photos/japan/tokyo11.JPG" alt="Tsukiji Market" width={300}
                 height={100} />
-            <Image src="/photos/japan/tokyo11.JPG" alt="" width={300}
+            <Image src="/photos/japan/tokyo16.jpg" alt="Bamboo Forest" width={125}
                 height={100} />
-            <Image src="/photos/japan/tokyo16.jpg" alt="" width={125}
+            <Image src="/photos/japan/tokyo17.jpg" alt="" width={300}
+                height={100} />
+            <Image src="/photos/japan/tokyo18.jpg" alt="Tsukiji stall" width={130}
                 height={100} />
 
-            <Image src="/photos/japan/japan1.jpg" alt="" width={300}
+            {/* KAMAKURA */}
+            <h1 className="text-xl font-bold py-20">Kamakura</h1>
+            <Image src="/photos/japan/japan1.jpg" alt="Great Buddha" width={300}
                 height={100} />
-            <Image src="/photos/japan/japan2.JPG" alt="" width={300}
+            <Image src="/photos/japan/tokyo13.jpg" alt="Lotus Flower" width={300}
                 height={100} />
+            <Image src="/photos/japan/kamakura1.jpg" alt="" width={300}
+                height={100} />
+            <Image src="/photos/japan/kamakura2.jpg" alt="" width={130}
+                height={100} />
+
+            {/* HAKONE */}
+            <h1 className="text-xl font-bold py-20">Hakone</h1>
             <Image src="/photos/japan/japan3.JPG" alt="" width={300}
                 height={100} />
-            <Image src="/photos/japan/japan7.jpg" alt="" width={125}
+            <Image src="/photos/japan/japan7.jpg" alt="" width={130}
                 height={100} />
             <Image src="/photos/japan/japan4.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/japan/japan5.jpg" alt="" width={300}
                 height={100} />
-            <Image src="/photos/japan/japan6.jpg" alt="" width={125}
+            <Image src="/photos/japan/japan6.jpg" alt="" width={130}
+                height={100} />
+            <Image src="/photos/japan/japan8.jpg" alt="" width={130}
                 height={100} />
 
+            {/* KYOTO */}
+            <h1 className="text-xl font-bold py-20">Kyoto</h1>
             <Image src="/photos/japan/kyoto1.jpg" alt="" width={300}
                 height={100} />
             <Image src="/photos/japan/kyoto2.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/japan/kyoto3.JPG" alt="" width={300}
                 height={100} />
-            <Image src="/photos/japan/kyoto4.JPG" alt="" width={300}
-                height={100} />
-            <Image src="/photos/japan/kyoto5.jpg" alt="" width={125}
+            <Image src="/photos/japan/kyoto4.JPG" alt="Golden Pavilion" width={300}
                 height={100} />
             <Image src="/photos/japan/kyoto6.jpg" alt="" width={300}
                 height={100} />
             <Image src="/photos/japan/kyoto7.jpg" alt="" width={300}
                 height={100} />
-            <Image src="/photos/japan/kyoto8.jpg" alt="" width={125}
+            <Image src="/photos/japan/kyoto8.jpg" alt="" width={130}
                 height={100} />
             <Image src="/photos/japan/kyoto9.jpg" alt="" width={300}
                 height={100} />
-        </div>
 
+            <h1 className="text-xl font-bold py-20">Nara</h1>
+            <Image src="/photos/japan/japan2.JPG" alt="" width={300}
+                height={100} />
+            <Image src="/photos/japan/kyoto5.jpg" alt="Nara Deer" width={130}
+                height={100} />
+        </div>
     </div>
 }
