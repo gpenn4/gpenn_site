33319a8a2998cf1e27acc311dc6d5f01af769504
gpenn4 <georgiapennington4@gmail.com>
2025-10-21 22:25:17 -0400

    Style: iceland page

--- gpenn-site/src/app/photos/ice/page.tsx
+++ gpenn-site/src/app/photos/ice/page.tsx
@@ -3,23 +3,29 @@ import Image from 'next/image'
 export default function IcelandPage() {
     return <div>
         <div className="gallery gap-4">
+            <h1 className="text-xl font-bold py-20">Reykjavík</h1>
             <Image src="/photos/iceland/reyk1.jpg" alt="" width={300}
                 height={100} />
-            <Image src="/photos/iceland/reyk2.jpg" alt="" width={300}
+            <Image src="/photos/iceland/reyk10.jpg" alt="" width={130}
                 height={100} />
-            <Image src="/photos/iceland/reyk5.JPG" alt="" width={300}
+            <Image src="/photos/iceland/reyk9.jpg" alt="Ijornin" width={300}
                 height={100} />
-            <Image src="/photos/iceland/reyk6.JPG" alt="" width={300}
+            <Image src="/photos/iceland/reyk2.jpg" alt="Ijornin" width={300}
                 height={100} />
-            <Image src="/photos/iceland/reyk7.JPG" alt="" width={125}
+            {/* <Image src="/photos/iceland/reyk6.JPG" alt="Ijornin" width={300}
+                height={100} /> */}
+            <Image src="/photos/iceland/reyk7.JPG" alt="" width={130}
                 height={100} />
             <Image src="/photos/iceland/reyk3.jpg" alt="" width={300}
                 height={100} />
-            <Image src="/photos/iceland/reyk4.JPG" alt="" width={300}
+            <Image src="/photos/iceland/reyk4.JPG" alt="Harpa" width={300}
                 height={100} />
-            <Image src="/photos/iceland/reyk8.JPG" alt="" width={125}
+            <Image src="/photos/iceland/reyk8.JPG" alt="" width={130}
                 height={100} />
-                
+            <Image src="/photos/iceland/reyk5.jpg" alt="" width={130}
+                height={100} />
+
+            <h1 className="text-xl font-bold py-20">Núpsstaður</h1>
             <Image src="/photos/iceland/ice7.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/iceland/ice1.jpg" alt="" width={300}
@@ -30,39 +36,53 @@ export default function IcelandPage() {
                 height={100} />
             <Image src="/photos/iceland/ice6.JPG" alt="" width={300}
                 height={100} />
+            <Image src="/photos/iceland/nup1.jpg" alt="" width={130}
+                height={100} />
+            <Image src="/photos/iceland/nup2.jpg" alt="" width={300}
+                height={100} />
+            <Image src="/photos/iceland/nup3.jpg" alt="" width={130}
+                height={100} />
+
+            <h1 className="text-xl font-bold py-20">Fjallsárlón</h1>
             <Image src="/photos/iceland/ice4.JPG" alt="" width={300}
                 height={100} />
-            {/* <Image src="/photos/iceland/ice9.jpg" alt="" width={300}
-                height={100} /> */}
             <Image src="/photos/iceland/ice10.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/iceland/ice11.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/iceland/ice12.JPG" alt="" width={300}
                 height={100} />
+
+            <h1 className="text-xl font-bold py-20">Thingvellir National Park</h1>
             <Image src="/photos/iceland/ice2.HEIC" alt="" width={300}
                 height={100} />
             <Image src="/photos/iceland/ice20.heic" alt="" width={125}
-                height={100} />    
+                height={100} />
+
             <Image src="/photos/iceland/ice5.JPG" alt="" width={300}
                 height={100} />
-            <Image src="/photos/iceland/ice13.JPG" alt="" width={300}
-                height={100} />
-            <Image src="/photos/iceland/ice14.JPG" alt="" width={300}
-                height={100} />
-            <Image src="/photos/iceland/ice15.JPG" alt="" width={300}
+            <Image src="/photos/iceland/ice15.JPG" alt="Horse" width={300}
                 height={100} />
             <Image src="/photos/iceland/ice16.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/iceland/ice17.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/iceland/ice18.JPG" alt="" width={300}
-                height={100} />     
-            <Image src="/photos/iceland/ice19.JPG" alt="" width={125}
-                height={100} />          
-            <Image src="/photos/iceland/ice21.JPG" alt="" width={125}
-                height={100} />    
-        </div>
+                height={100} />
+            <Image src="/photos/iceland/ice19.JPG" alt="" width={130}
+                height={100} />
+            <Image src="/photos/iceland/ice21.JPG" alt="" width={130}
+                height={100} />
+            <Image src="/photos/iceland/ice22.jpg" alt="" width={130}
+                height={100} />
+            <Image src="/photos/iceland/ice23.JPG" alt="people under waterfall" width={300}
+                height={100} />
 
+            <h1 className="text-xl font-bold py-20">Diamond Beach</h1>
+            <Image src="/photos/iceland/ice13.JPG" alt="" width={300}
+                height={100} />
+            <Image src="/photos/iceland/ice14.JPG" alt="" width={300}
+                height={100} />
+        </div>
     </div>
 }
