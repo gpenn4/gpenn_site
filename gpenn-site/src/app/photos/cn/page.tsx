7718dc5b303e1689429056a28375aed4415a3192
gpenn4 <georgiapennington4@gmail.com>
2025-10-21 23:20:25 -0400

    Style: china page

--- gpenn-site/src/app/photos/cn/page.tsx
+++ gpenn-site/src/app/photos/cn/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 export default function ChinaPage() {
     return <div>
         <div className="gallery gap-4">
+            <h1 className="text-xl font-bold py-20">Hong Kong</h1>
             <Image src="/photos/china/hk2.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/china/hk6.JPG" alt="" width={125}
@@ -16,6 +17,5 @@ export default function ChinaPage() {
             <Image src="/photos/china/hk5.jpg" alt="" width={125}
                 height={100} />
         </div>
-
     </div>
 }
