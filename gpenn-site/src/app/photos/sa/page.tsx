b6651e4c5ba8c5ec2d1cb41ac9688d26ff168ffe
gpenn4 <georgiapennington4@gmail.com>
2025-10-21 23:52:06 -0400

    Style: south africa page

--- gpenn-site/src/app/photos/sa/page.tsx
+++ gpenn-site/src/app/photos/sa/page.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 export default function SouthAfricaPage() {
     return <div>
         <div className="gallery gap-4">
+            {/* CAPE TOWN */}
+            <h1 className="text-xl font-bold py-20">Cape Town</h1>
             <Image src="/photos/southafrica/capetown1.jpg" alt="" width={300}
                 height={100} />
             <Image src="/photos/southafrica/capetown2.JPG" alt="" width={300}
@@ -15,13 +17,18 @@ export default function SouthAfricaPage() {
                 height={100} />
             <Image src="/photos/southafrica/capetown5.JPG" alt="" width={300}
                 height={100} />
+            <Image src="/photos/southafrica/capetown7.jpg" alt="" width={125}
+                height={100} />
 
             <Image src="/photos/southafrica/sa1.JPG" alt="" width={300}
                 height={100} />
 
+            <h1 className="text-xl font-bold py-20">Aquila Private Game Reserve</h1>
             <Image src="/photos/southafrica/safari1.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/southafrica/safari2.JPG" alt="" width={300}
+                height={100} />
+            <Image src="/photos/southafrica/safari9.jpg" alt="" width={125}
                 height={100} />
             <Image src="/photos/southafrica/safari5.JPG" alt="" width={300}
                 height={100} />
@@ -35,7 +42,10 @@ export default function SouthAfricaPage() {
                 height={100} />     
             <Image src="/photos/southafrica/safari8.jpg" alt="" width={300}
                 height={100} />     
+            <Image src="/photos/southafrica/safari10.jpg" alt="" width={125}
+                height={100} />
+            <Image src="/photos/southafrica/safari11.jpg" alt="" width={300}
+                height={100} />
         </div>
-
     </div>
 }
