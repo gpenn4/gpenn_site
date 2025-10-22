6e17de5a254a6e4dc275dee8d8fdfbe3a04d174e
gpenn4 <georgiapennington4@gmail.com>
2025-10-22 11:15:33 -0400

    Style: spain page

--- gpenn-site/src/app/photos/es/page.tsx
+++ gpenn-site/src/app/photos/es/page.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 export default function SpainPage() {
     return <div>
         <div className="gallery gap-4">
+            {/* MALLORCA */}
+            <h1 className="text-xl font-bold py-20">Mallorca</h1>
             <Image src="/photos/spain/mallorca1.jpg" alt="" width={300}
                 height={100} />
             <Image src="/photos/spain/mallorca3.jpg" alt="" width={300}
@@ -12,17 +14,19 @@ export default function SpainPage() {
             <Image src="/photos/spain/mallorca4.jpg" alt="" width={300}
                 height={100} />
 
+            {/* BARCELONA */}
+            <h1 className="text-xl font-bold py-20">Barcelona</h1>
             <Image src="/photos/spain/barcelona1.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/spain/barcelona2.jpg" alt="" width={300}
                 height={100} />
-            {/* <Image src="/photos/spain/barcelona3.jpg" alt="" width={300}
-                height={100} /> */}
             <Image src="/photos/spain/barcelona4.jpg" alt="" width={300}
                 height={100} />
             <Image src="/photos/spain/barcelona6.JPG" alt="" width={300}
                 height={100} />
 
+            {/* TENERIFE */}
+            <h1 className="text-xl font-bold py-20">Tenerife</h1>
             <Image src="/photos/spain/tenerife1.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/spain/tenerife2.JPG" alt="" width={300}
@@ -35,6 +39,8 @@ export default function SpainPage() {
                 height={100} />
             <Image src="/photos/spain/tenerife7.JPG" alt="" width={300}
                 height={100} />
+            <Image src="/photos/spain/tenerife11.JPG" alt="" width={125}
+                height={100} />
             <Image src="/photos/spain/tenerife4.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/spain/tenerife8.JPG" alt="" width={300}
@@ -44,6 +50,8 @@ export default function SpainPage() {
             <Image src="/photos/spain/tenerife10.JPG" alt="" width={125}
                 height={100} />
 
+            {/* LANZAROTE */}
+            <h1 className="text-xl font-bold py-20">Lanzarote</h1>
             <Image src="/photos/spain/lanzarote1.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/spain/lanzarote2.JPG" alt="" width={300}
@@ -62,6 +70,8 @@ export default function SpainPage() {
                 height={100} />
             <Image src="/photos/spain/lanzarote9.JPG" alt="" width={300}
                 height={100} />
+            <Image src="/photos/spain/lanzarote15.JPG" alt="Beach Stairs" width={125}
+                height={100} />        
             <Image src="/photos/spain/lanzarote10.JPG" alt="" width={300}
                 height={100} />
             <Image src="/photos/spain/lanzarote11.JPG" alt="" width={300}
@@ -71,7 +81,7 @@ export default function SpainPage() {
             <Image src="/photos/spain/lanzarote13.JPG" alt="" width={300}
                 height={100} /> 
             <Image src="/photos/spain/lanzarote14.JPG" alt="" width={125}
-                height={100} />           
+                height={100} />
         </div>
 
     </div>
