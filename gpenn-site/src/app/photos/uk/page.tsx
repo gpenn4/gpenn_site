5448aa903c141f9f0873ef8a135f7b9aaaefdaec
gpenn4 <georgiapennington4@gmail.com>
2025-12-15 13:33:41 +0000

    Style: uk page

--- gpenn-site/src/app/photos/uk/page.tsx
+++ gpenn-site/src/app/photos/uk/page.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image'
 
 export default function UKPage() {
   return <div>
-
     <div className="gallery gap-4">
       {/* FIFE */}
       <h1 className="text-xl font-bold py-20">Fife</h1>
@@ -10,9 +9,9 @@ export default function UKPage() {
         height={100} />
       <Image src="/photos/scotland/scotland2.JPG" alt="couple on bench" width={300}
         height={100} />
-      <Image src="/photos/scotland/scotland3.JPG" alt="" width={300}
+      <Image src="/photos/scotland/scotland3.JPG" alt="walking away from the lighthouse" width={300}
         height={100} />
-      <Image src="/photos/scotland/scotland9.JPG" alt="" width={300}
+      <Image src="/photos/scotland/scotland9.JPG" alt="view from Lady's Tower" width={300}
         height={100} />
       <Image src="/photos/scotland/scotland10.JPG" alt="" width={300}
         height={100} />
@@ -38,8 +37,6 @@ export default function UKPage() {
         height={100} />
       <Image src="/photos/scotland/scotland24.JPG" alt="" width={300}
         height={100} />
-      <Image src="/photos/scotland/scotland25.JPG" alt="brunch" width={300}
-        height={100} />
       <Image src="/photos/scotland/scotland27.JPG" alt="" width={300}
         height={100} />
       <Image src="/photos/scotland/scotland28.JPG" alt="" width={300}
@@ -48,26 +45,29 @@ export default function UKPage() {
         height={100} />
       <Image src="/photos/scotland/scotland30.JPG" alt="" width={300}
         height={100} />
-      <Image src="/photos/scotland/scotland41.jpg" alt="" width={125}
+      <Image src="/photos/scotland/scotland31.JPG" alt="aberdour gardens" width={300}
         height={100} />
-      <Image src="/photos/scotland/scotland31.JPG" alt="" width={300}
-        height={100} />
-      <Image src="/photos/scotland/scotland32.JPG" alt="" width={300}
+      <Image src="/photos/scotland/scotland32.JPG" alt="aberdour doocot" width={300}
         height={100} />
       <Image src="/photos/scotland/scotland36.JPG" alt="end of the rainbow" width={300}
         height={100} />
-      <Image src="/photos/scotland/scotland40.JPG" alt="" width={125}
+      <Image src="/photos/scotland/scotland40.JPG" alt="picnic table" width={125}
         height={100} />
-      <Image src="/photos/scotland/scotland39.JPG" alt="" width={125}
+      <Image src="/photos/scotland/scotland39.JPG" alt="garden gate" width={125}
         height={100} />
-      <Image src="/photos/scotland/scotland33.JPG" alt="" width={300}
+      <Image src="/photos/scotland/scotland33.JPG" alt="aberdour castle" width={300}
         height={100} />
-      <Image src="/photos/scotland/scotland42.jpg" alt="" width={125}
+      <Image src="/photos/scotland/scotland42.jpg" alt="abbey ruins" width={125}
         height={100} />
-      <Image src="/photos/scotland/scotland43.jpg" alt="" width={125}
+      <Image src="/photos/scotland/scotland43.jpg" alt="tree cave" width={125}
         height={100} />
 
-      {/* HIGHLANDS */}
+      {/* CENTRAL SCOTLAND */}
+      <h1 className="text-xl font-bold py-20">Central Scotland</h1>
+      <Image src="/photos/scotland/scotland41.jpg" alt="stirling castle gates" width={125}
+        height={100} />
+
+      {/* THE HIGHLANDS */}
       <h1 className="text-xl font-bold py-20">The Highlands</h1>
       <Image src="/photos/scotland/scotland4.JPG" alt="" width={300}
         height={100} />
@@ -88,23 +88,25 @@ export default function UKPage() {
 
       {/* ST ANDREWS */}
       <h1 className="text-xl font-bold py-20">St Andrews</h1>
-      <Image src="/photos/scotland/scotland26.JPG" alt="" width={300}
+      <Image src="/photos/scotland/scotland26.JPG" alt="Syl and Ana" width={300}
+        height={100} />
+      <Image src="/photos/scotland/scotland25.JPG" alt="brunch" width={300}
         height={100} />
       <Image src="/photos/scotland/scotland37.JPG" alt="sta castle ruins" width={300}
         height={100} />
-      <Image src="/photos/scotland/scotland38.JPG" alt="east sands" width={300}
-        height={100} />
+      {/* <Image src="/photos/scotland/scotland38.JPG" alt="east sands" width={300}
+        height={100} /> */}
       <Image src="/photos/scotland/sta6.jpg" alt="" width={300}
         height={100} />
       <Image src="/photos/scotland/sta17.JPG" alt="Jesse Grad" width={130}
         height={100} />
-      <Image src="/photos/scotland/sta7.JPG" alt="" width={300}
+      <Image src="/photos/scotland/sta7.JPG" alt="Daisy and Katia" width={300}
         height={100} />
-      <Image src="/photos/scotland/sta1.JPG" alt="" width={300}
+      <Image src="/photos/scotland/sta1.JPG" alt="Opening Ball" width={300}
         height={100} />
       <Image src="/photos/scotland/sta2.jpg" alt="Octopus Tapestry" width={300}
         height={100} />
-      <Image src="/photos/scotland/sta14.JPG" alt="" width={125}
+      <Image src="/photos/scotland/sta14.JPG" alt="" width={130}
         height={100} />
       <Image src="/photos/scotland/sta3.jpg" alt="" width={300}
         height={100} />
@@ -112,21 +114,25 @@ export default function UKPage() {
         height={100} />
       <Image src="/photos/scotland/sta8.JPG" alt="" width={300}
         height={100} />
-      <Image src="/photos/scotland/sta15.JPG" alt="" width={125}
+      <Image src="/photos/scotland/sta15.JPG" alt="" width={130}
         height={100} />
       <Image src="/photos/scotland/sta10.jpg" alt="" width={300}
         height={100} />
       {/* <Image src="/photos/scotland/sta18.JPG" alt="" width={300}
         height={100} /> */}
-      <Image src="/photos/scotland/sta11.jpg" alt="" width={125}
+      <Image src="/photos/scotland/sta11.jpg" alt="" width={130}
         height={100} />
       <Image src="/photos/scotland/sta12.JPG" alt="" width={300}
         height={100} />
       <Image src="/photos/scotland/sta13.jpg" alt="" width={300}
         height={100} />
-      <Image src="/photos/scotland/sta16.jpg" alt="" width={125}
+      <Image src="/photos/scotland/sta16.jpg" alt="" width={130}
         height={100} />
-      <Image src="/photos/scotland/sta19.JPG" alt="" width={125}
+      <Image src="/photos/scotland/sta19.JPG" alt="" width={130}
+        height={100} />
+      {/* <Image src="/photos/scotland/sta20.jpg" alt="" width={300}
+        height={100} /> */}
+      <Image src="/photos/scotland/sta21.jpg" alt="" width={300}
         height={100} />
 
       {/* EDINBURGH */}
