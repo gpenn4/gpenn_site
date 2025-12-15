fdc721682c285b51ea714fba50e98e2fc4176d23
gpenn4 <georgiapennington4@gmail.com>
2025-12-15 13:35:18 +0000

    Style: France page

--- gpenn-site/src/app/photos/fr/page.tsx
+++ gpenn-site/src/app/photos/fr/page.tsx
@@ -4,11 +4,11 @@ export default function FrancePage() {
     return <div>
         <div className="gallery gap-4">
             <h1 className="text-xl font-bold py-20">Paris</h1>
-            <Image src="/photos/france/paris1.jpg" alt="" width={300}
+            <Image src="/photos/france/paris1.jpg" alt="Jesse Louvre" width={300}
                 height={100} />
-            <Image src="/photos/france/paris3.JPG" alt="" width={300}
+            <Image src="/photos/france/paris3.JPG" alt="Louvre pyramid" width={300}
                 height={100} />
-            <Image src="/photos/france/paris11.JPG" alt="" width={300}
+            <Image src="/photos/france/paris11.JPG" alt="Louvre statues" width={300}
                 height={100} />
             <Image src="/photos/france/paris2.JPG" alt="" width={300}
                 height={100} />
@@ -20,21 +20,25 @@ export default function FrancePage() {
                 height={100} />
             <Image src="/photos/france/paris5.JPG" alt="" width={300}
                 height={100} />
-            <Image src="/photos/france/paris6.JPG" alt="" width={300}
+            <Image src="/photos/france/paris6.JPG" alt="cab window" width={300}
                 height={100} />
-            <Image src="/photos/france/paris7.JPG" alt="" width={300}
+            <Image src="/photos/france/paris7.JPG" alt="Hopie" width={300}
                 height={100} />
-            <Image src="/photos/france/paris8.JPG" alt="" width={300}
+            <Image src="/photos/france/paris8.JPG" alt="Jesse" width={300}
                 height={100} />
-            <Image src="/photos/france/paris12.JPG" alt="" width={300}
+            <Image src="/photos/france/paris12.JPG" alt="stained glass" width={300}
                 height={100} />
             <Image src="/photos/france/paris13.jpg" alt="" width={300}
                 height={100} />
-            <Image src="/photos/france/paris15.jpg" alt="" width={300}
+            <Image src="/photos/france/paris15.jpg" alt="couple at cafe" width={300}
                 height={100} />
-            <Image src="/photos/france/paris16.jpg" alt="" width={300}
+            <Image src="/photos/france/paris16.jpg" alt="man in window" width={300}
                 height={100} />
-            <Image src="/photos/france/paris17.jpg" alt="" width={300}
+            <Image src="/photos/france/paris17.jpg" alt="kid at Louvre" width={300}
+                height={100} />
+            <Image src="/photos/france/paris18.jpg" alt="couple at museum, October 2022" width={300}
+                height={100} />
+            <Image src="/photos/france/paris19.jpg" alt="garden statue, October 2022" width={300}
                 height={100} />
 
             <h1 className="text-xl font-bold py-20">Nice</h1>
