278ad63344e2819ec3b2a0ffc159172551e142c5
gpenn4 <georgiapennington4@gmail.com>
2025-12-15 13:34:28 +0000

    Style: morocco page

--- gpenn-site/src/app/photos/mar/page.tsx
+++ gpenn-site/src/app/photos/mar/page.tsx
@@ -42,6 +42,31 @@ export default function MoroccoPage() {
                 height={100} />
             <Image src="/photos/morocco/marrakech19.JPG" alt="" width={300}
                 height={100} />
+            <h1 className="text-xl font-bold py-20">Tangier</h1>
+            <Image src="/photos/morocco/tan1.JPG" alt="" width={125}
+                height={100} />
+            <Image src="/photos/morocco/tan2.JPG" alt="" width={125}
+                height={100} />
+            <Image src="/photos/morocco/tan3.JPG" alt="" width={300}
+                height={100} />
+            <Image src="/photos/morocco/tan4.JPG" alt="" width={300}
+                height={100} />
+            <Image src="/photos/morocco/tan5.JPG" alt="" width={125}
+                height={100} />
+            <Image src="/photos/morocco/tan6.JPG" alt="" width={125}
+                height={100} />
+            <Image src="/photos/morocco/tan7.JPG" alt="" width={300}
+                height={100} />
+            <Image src="/photos/morocco/tan8.JPG" alt="" width={300}
+                height={100} />
+            <Image src="/photos/morocco/tan9.JPG" alt="" width={300}
+                height={100} />
+            <Image src="/photos/morocco/tan10.JPG" alt="" width={300}
+                height={100} />
+            <Image src="/photos/morocco/tan11.JPG" alt="" width={125}
+                height={100} />
+            <Image src="/photos/morocco/tan12.JPG" alt="" width={125}
+                height={100} />
         </div>
     </div>
 }
